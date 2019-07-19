56a2cb05fc0471552613e4a2bc4cce28e1f3cb3f
jomno <jake8955@naver.com>
2019-07-20 01:35:44 +0900

    fixed version

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,36 +2,36 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
-  
+
   def show
     @post = Post.find(params[:id])
   end
 
   def new
   end
-  
+
   def create
     @post = Post.new(title: params[:title], content: params[:content])
     @post.save
-    
-    redirect_to post_path(@post)
+
+    redirect_to "/posts/index"
   end
 
   def edit
     @post = Post.find(params[:id])
   end
-  
+
   def update
     @post = Post.find(params[:id])
     @post.update(title: params[:title], content: params[:content])
-    
-    redirect_to post_path(@post)
+
+    redirect_to "/posts/index"
   end
-  
+
   def destroy
     @post = Post.find(params[:id])
     @post.destroy
-    
-    redirect_to posts_path
+
+    redirect_to "/posts/index"
   end
 end
