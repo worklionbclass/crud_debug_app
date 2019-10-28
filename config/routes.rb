dd9ab05a04bcca4e888ced56ecbefcf87040065f
jomno <jake8955@naver.com>
2019-10-28 22:53:30 +0900

    bug deleted

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,8 @@ Rails.application.routes.draw do
   get "posts/new" => "posts#new"
   post "posts/create" => "posts#create"
   get "posts/:id/show" => "posts#show"
-  get "post/edit" => "posts#edit"
+  get "posts/edit" => "posts#edit"
   post "posts/:id/update" => "posts#update"
-  get "posts/destroy" => "posts#destroy"
+  get "posts/:id/destroy" => "posts#destroy"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
